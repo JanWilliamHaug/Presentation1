abebfe5f8645f704721ec3c497dcd2f3aad914ac
JanWilliamHaug <71844869+JanWilliamHaug@users.noreply.github.com>
2022-10-13 18:58:24 -0700

    small changes to strings

--- Gui.py
+++ Gui.py
@@ -19,7 +19,7 @@ lbl.grid()
 
 def find_(paragraph_keyword, draft_keyword, paragraph):
     if paragraph_keyword in paragraph.text:
-        print("found tag:", draft_keyword)
+        print("Keywords found:", draft_keyword)
         #prints out "found tag:" whenever a tag is found
 
 # function to display text when
@@ -27,23 +27,21 @@ def find_(paragraph_keyword, draft_keyword, paragraph):
 def clicked():
     lbl.configure(text = "Excel report has been generated")
 
-    document = docx.Document('test10.docx')
-    print(document.paragraphs[0].text)
-    print(document.paragraphs[1].text)
+    document = docx.Document('SRS_ACE_Pump_X01.docx')
 
-    product = "TARGEST"
-    coFounder1 = "Jan"
-    coFounder2 = "Adrian"
-    coFounder3 = "Stephania"
-    name = "NAME"
-    title = "TITLE"
-    title2 = "Co-Founder"
+    keyword1 = "PUMP"
+    keyword2 = "ACE"
+    keyword3 = "PRS"
+    keyword4 = "SRS"
+    name = "Keyword"
+    title = "Tag"
+    #title2 = "Co-Founder"
 
     for paragraph in document.paragraphs:
-        find_("product", product, paragraph)
-        find_("Co-Founder1", coFounder1, paragraph)
-        find_("Co-Founder2", coFounder2, paragraph)
-        find_("Co-Founder3", coFounder3, paragraph)
+        find_("PUMP", keyword1, paragraph)
+        find_("ACE", keyword2, paragraph)
+        find_("PRS", keyword3, paragraph)
+        find_("SRS", keyword4, paragraph)
 
     excelFile = xw.Book()                #Creates an empty excel file
     excelFile.save('report.xlsx')        #Saves that excel file as "data1"
@@ -51,24 +49,22 @@ def clicked():
     ws1 = excelFile.sheets['Sheet1']
     ws1.range('A1').value = name         #Adds the string "Name" to A2
     ws1.range('B1').value = title        #Adds the string "Title" to B1
-    ws1.range('A2').value = coFounder1   #Adds name of Co-Founder 1 to A2
-    ws1.range('A3').value = coFounder2   #Adds name of Co-Founder 2 to A3
-    ws1.range('A4').value = coFounder3   #Adds name of Co-Founder 3 to A3
-    ws1.range('C1').value = product      #Adds name of the product to C1
-    ws1.range('B2:B4').value = title2    #Adds  titles
+    ws1.range('A2').value = keyword1   #Adds name of Co-Founder 1 to A2
+    ws1.range('A3').value = keyword2   #Adds name of Co-Founder 2 to A3
+    ws1.range('B2').value = keyword3   #Adds name of Co-Founder 3 to A3
+    ws1.range('B3').value = keyword4      #Adds name of the product to C1
+    #ws1.range('B2:B4').value = title2    #Adds  titles
 
-    lbl2 = Label(root, text = "Tags:")
+    lbl2 = Label(root, text = "Keywords found:")
     lbl2.grid()
-    lbl3 = Label(root, text = coFounder1)
+    lbl3 = Label(root, text = keyword1)
     lbl3.grid()
-    lbl4 = Label(root, text = coFounder2)
+    lbl4 = Label(root, text = keyword2)
     lbl4.grid()
-    lbl5 = Label(root, text = coFounder3)
+    lbl5 = Label(root, text = keyword3)
     lbl5.grid()
-
-
-
-
+    lbl6 = Label(root, text = keyword4)
+    lbl6.grid()
 
 
 # button widget with red color text
